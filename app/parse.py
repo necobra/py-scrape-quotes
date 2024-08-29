96e5c4108c4bff4f46b3a9535c0acd44bcffa8f9
necobra <necobrane105@gmail.com>
2024-08-29 16:53:21 +0300

    better structure + tqdm

--- app/parse.py
+++ app/parse.py
@@ -1,10 +1,11 @@
 import csv
 from dataclasses import dataclass, astuple
+from typing import Generator, List
 from urllib.parse import urljoin
 
 import requests
-from bs4 import BeautifulSoup
-
+from bs4 import BeautifulSoup, Tag
+from tqdm import tqdm
 
 BASE_URL = "https://quotes.toscrape.com/"
 
@@ -16,34 +17,56 @@ class Quote:
     tags: list[str]
 
 
-def parse_single_quote(quote_soup: BeautifulSoup) -> Quote:
+def parse_quote_details(quote: Tag) -> Quote:
+    """
+    Parse and return the details of a book.
+    Args:
+        quote (Tag): A BeautifulSoup Tag object representing a book.
+    Returns:
+        Book: An instance of the Book dataclass containing book details.
+    """
     return Quote(
-        text=quote_soup.select_one(".text").text[1:-1],
-        author=quote_soup.select_one(".author").text,
-        tags=[tag_soup.text for tag_soup in quote_soup.select(".tag")]
+        text=quote.select_one(".text").text[1:-1],
+        author=quote.select_one(".author").text,
+        tags=[tag_soup.text for tag_soup in quote.select(".tag")]
     )
 
+def fetch_page_content(url: str) -> bytes | None:
+    """
+    Fetch the content of a webpage.
+    Args:
+        url (str): The URL of the webpage to fetch.
+    Returns:
+        Optional[str]: The content of the page, or None if an error occurs.
+    """
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.content
+    except requests.RequestException as e:
+        print(f"Error fetching page content: {e}")
 
-def get_single_page_quotes(page_soup: BeautifulSoup) -> list[Quote]:
-    quotes = page_soup.select(".quote")
-    return [parse_single_quote(quote) for quote in quotes]
 
-
-def get_all_quotes() -> list[Quote]:
-    i = 1
-    quotes = []
+def page_generator(url: str) -> Generator[BeautifulSoup, None, None]:
+    """
+    Generate a BeautifulSoup object from the content of a webpage.
+    Args:
+        url (str): The URL of the webpage.
+    Yields:
+        BeautifulSoup: A BeautifulSoup object representing the parsed HTML content.
+    """
+    page_counter = 1
     while True:
-        page_url = urljoin(BASE_URL, f"/page/{i}")
-        request = requests.get(page_url)
-        request.raise_for_status()
-        soup = BeautifulSoup(request.content, "html.parser")
+        page_url = urljoin(url, f"/page/{page_counter}/")
+        page_content = fetch_page_content(page_url)
+        page_soup = BeautifulSoup(page_content, "html.parser")
 
-        quotes += get_single_page_quotes(soup)
+        yield page_soup
 
-        if not soup.select_one(".next"):
+        if not page_soup.select_one(".next"):
             break
-        i += 1
-    return quotes
+
+        page_counter += 1
 
 
 def write_quotes_to_csv(quotes: list[Quote], output_csv_path: str) -> None:
@@ -52,8 +75,30 @@ def write_quotes_to_csv(quotes: list[Quote], output_csv_path: str) -> None:
         write.writerows([astuple(quote) for quote in quotes])
 
 
+def extract_books(soup: BeautifulSoup) -> List[Quote]:
+    """
+    Extract the list of books from the parsed HTML content.
+    Args:
+        soup (BeautifulSoup): A BeautifulSoup object of the webpage.
+    Returns:
+        List[Book]: A list of Book dataclass instances with book details.
+    """
+    quotes = soup.select(".quote")
+    return [parse_quote_details(quote) for quote in quotes]
+
+
+def scrape_quotes() -> List[Quote]:
+    """
+    Scrape the books listing page for individual book details.
+    """
+    quotes = []
+    for page in tqdm(page_generator(BASE_URL)):
+        quotes.extend(extract_books(page))
+    return quotes
+
+
 def main(output_csv_path: str) -> None:
-    quotes = get_all_quotes()
+    quotes = scrape_quotes()
     write_quotes_to_csv(quotes, output_csv_path)
 
 
