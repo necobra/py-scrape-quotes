e82ee5dcc0c28330f30ce4623811fcc393fba5aa
necobra <necobrane105@gmail.com>
2024-08-29 16:39:09 +0300

    git Solution

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,11 @@
-from dataclasses import dataclass
+import csv
+from dataclasses import dataclass, astuple
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://quotes.toscrape.com/"
 
 
 @dataclass
@@ -7,9 +14,41 @@ class Quote:
     author: str
     tags: list[str]
 
+def parse_single_quote(quote_soup: BeautifulSoup) -> Quote:
+    return Quote(
+        text=quote_soup.select_one(".text").text[1:-1],
+        author=quote_soup.select_one(".author").text,
+        tags=[tag_soup.text for tag_soup in quote_soup.select(".tag")]
+    )
+
+def get_single_page_quotes(page_soup: BeautifulSoup) -> list[Quote]:
+    quotes = page_soup.select(".quote")
+    return [parse_single_quote(quote) for quote in quotes]
+
+def get_all_quotes() -> list[Quote]:
+    i = 1
+    quotes = []
+    while True:
+        page_url = urljoin(BASE_URL, f"/page/{i}")
+        request = requests.get(page_url)
+        request.raise_for_status()
+        soup = BeautifulSoup(request.content, "html.parser")
+
+        quotes += get_single_page_quotes(soup)
+
+        if not soup.select_one(".next"):
+            break
+        i += 1
+    return quotes
+
+def write_quotes_to_csv(quotes: list[Quote], output_csv_path: str):
+    with open(output_csv_path, "w", encoding="utf-8") as file:
+        write = csv.writer(file)
+        write.writerows([astuple(quote) for quote in quotes])
 
 def main(output_csv_path: str) -> None:
-    pass
+    quotes = get_all_quotes()
+    write_quotes_to_csv(quotes, output_csv_path)
 
 
 if __name__ == "__main__":
